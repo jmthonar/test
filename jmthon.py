9ba6611540b80e443e2c2211517735141fa1422f
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-23 21:32:01 +0300

    -

--- jmthon.py
+++ jmthon.py
@@ -1,1 +1,10 @@
+import asyncio
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 
+from config import APP_ID, API_HASH, STRING_SESSION
+
+jmthon = TelegramClient(StringSession(str(STRING_SESSION)), int(APP_ID), str(API_HASH)).start()
+
+print("تم التنصيب")
+jmthon.run_until_disconnected()
