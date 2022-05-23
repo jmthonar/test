715382b802f5617411df8ba499073f142c3891d8
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-23 10:15:02 +0300

    Update environment.py

--- environment.py
+++ environment.py
@@ -4,6 +4,7 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 DEFAULTUSERBIO = os.environ.get("DEFAULTUSERBIO", None)
+TZ = os.environ.get("TZ", "Asia/Baghdad")
 OWNER_ID = int(os.environ.get("OWNER_ID") or 0)
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
