ae9ffdb0d85e11d5338752a9caf0fcb5d1bc4331
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-23 07:06:37 +0300

    Update environment.py

--- environment.py
+++ environment.py
@@ -3,4 +3,4 @@ import os
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
-
+DEFAULTUSERBIO = os.environ.get("DEFAULTUSERBIO", None)
