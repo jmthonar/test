4ce2b71dacecd54acf7879fa9b1ddb697b5001cc
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-24 10:56:46 +0300

    Create اعادة تشغيل.py

---
+++ jmthon/plugins/اعادة تشغيل.py
@@ -0,0 +1,10 @@
+import sys
+from os import execl
+from telethon import events
+from jmthon import jmthon
+
+@jmthon.on(events.NewMessage(outgoing=True, pattern="^.اعادة تشغيل$"))
+async def _(event):
+    await event.edit("جار أعادة التشغيل الان انتظر من 2-5 دقائق")
+    await jmthon.disconnect()
+    execl(sys.executable, sys.executable, *sys.argv)
