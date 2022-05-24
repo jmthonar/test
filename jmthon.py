97edd5907e2d4c6204fe425d8463bcee6c86ca8b
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-24 06:12:53 +0300

    Update jmthon.py

--- jmthon.py
+++ jmthon.py
@@ -2,7 +2,7 @@ import asyncio
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from config import APP_ID, API_HASH, STRING_SESSION
-from plugins.gcast import broadcast
+from plugins.gcast import lolcast
 from plugins.autoprofile import autorzo
 
 jmthon = TelegramClient(StringSession(str(STRING_SESSION)), int(APP_ID), str(API_HASH)).start()
@@ -11,5 +11,5 @@ loop = asyncio.get_event_loop()
 
 print("تم التنصيب")
 loop.create_task(autorzo(jmthon))
-loop.create_task(broadcast(jmthon))
+loop.create_task(lolcast(jmthon))
 jmthon.run_until_disconnected()
