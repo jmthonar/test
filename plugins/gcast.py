ac4838308917f1c707d8c8837d1e6637bda532ba
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-24 06:59:13 +0300

    Update gcast.py

--- plugins/gcast.py
+++ plugins/gcast.py
@@ -1,5 +1,6 @@
 import asyncio
 from jmthon import jmthon 
+from telethon import events 
 
 GCAST_BLACKLIST = [
     -1001118102804,
@@ -11,60 +12,59 @@ DEVS = [
     2034443585,
 ]
 
-async def lolcast(jmthon):
-    @jmthon.on(events.NewMessage(outgoing=True, pattern=".للكروبات(?: |$)(.*)"))
-    async def gcast(event):
-        mohammad = event.pattern_match.group(1)
-        if mohammad:
-            msg = mohammad
-        elif event.is_reply:
-            msg = await event.get_reply_message()
-        else:
-            await event.edit(
-                "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
-            )
-            return
-        roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
-        er = 0
-        done = 0
-        async for x in event.client.iter_dialogs():
-            if x.is_group:
-                chat = x.id
-                try:
-                    if chat not in GCAST_BLACKLIST:
-                        await event.client.send_message(chat, msg)
-                        done += 1
-                except BaseException:
-                    er += 1
-        await roz.edit(
-            f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+@jmthon.on(events.NewMessage(outgoing=True, pattern=".للكروبات(?: |$)(.*)"))
+async def gcast(event):
+    jmthon = event.pattern_match.group(1)
+    if jmthon:
+        msg = jmthon
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
         )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
 
 
-    @jmthon.on(events.NewMessage(outgoing=True, pattern=".للخاص(?: |$)(.*)"))
-    async def gucast(event):
-        mohammad = event.pattern_match.group(1)
-        if mohammad:
-            msg = mohammad
-        elif event.is_reply:
-            msg = await event.get_reply_message()
-        else:
-            await event.edit(
-                "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
-            )
-            return
-        roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
-        er = 0
-        done = 0
-        async for x in event.client.iter_dialogs():
-            if x.is_user and not x.entity.bot:
-                chat = x.id
-                try:
-                    if chat not in DEVS:
-                        await event.client.send_message(chat, msg)
-                        done += 1
-                except BaseException:
-                    er += 1
-        await roz.edit(
-            f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+@jmthon.on(events.NewMessage(outgoing=True, pattern=".للخاص(?: |$)(.*)"))
+async def gucast(event):
+    jmthon = event.pattern_match.group(1)
+    if jmthon:
+        msg = jmthon
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
         )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                if chat not in DEVS:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
