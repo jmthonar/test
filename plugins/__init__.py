62b7f0e1f5d37098db824979707bddb94faf83e3
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-24 07:19:31 +0300

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,9 +1,9 @@
 import asyncio
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
-from config import APP_ID, API_HASH, STRING_SESSION
+from config import API_ID, API_HASH, STRING_SESSION
 
-jmthon = TelegramClient(StringSession(str(STRING_SESSION)), int(APP_ID), str(API_HASH)).start()
+jmthon = TelegramClient(StringSession(str(STRING_SESSION)), int(API_ID), str(API_HASH)).start()
 loop = asyncio.get_event_loop()
 
 
