4926184b374c60197d447d3591be4ac4179e274c
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-24 05:48:31 +0300

    Update autoprofile.py

--- plugins/autoprofile.py
+++ plugins/autoprofile.py
@@ -62,3 +62,15 @@ async def autoprofile(jmthon):
                 await asyncio.sleep(ex.seconds)
             await asyncio.sleep(DEL_TIME_OUT)
 
+    @jmthon.on(events.NewMessage(outgoing=True, pattern=".ذاتية"))
+    async def roz(bakar):
+        if not bakar.is_reply:
+            return await bakar.edit(
+                "**❃ يجب عليك الرد على صورة ذاتيه التدمير او صورة مؤقته**"
+            )
+        rr9r7 = await bakar.get_reply_message()
+        pic = await rr9r7.download_media()
+        await jmthon.send_file(
+            "me", pic, caption=f"**⪼ عزيزي هذه هي الصورة او الفيديو التي تم حفظه هنا**"
+        )
+        await bakar.delete()
