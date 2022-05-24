9b2e8fa33445e4fac94cdad7135e3738570d8a7c
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-24 08:26:26 +0300

    Update التكرار.py

--- jmthon/plugins/التكرار.py
+++ jmthon/plugins/التكرار.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 #from plugins.helpers import _rozutils
 from jmthon import jmthon
 
-@jmthon.on(events.NewMessage(outgoing=True, pattern=".تكرار (.*)"))
+@jmthon.on(events.NewMessage(outgoing=True, pattern="^.تكرار (.*)"))
 async def spammer(event):
     jasem = await event.get_reply_message()
     roz = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
@@ -23,7 +23,7 @@ async def spammer(event):
 
 
 
-@jmthon.on(events.NewMessage(outgoing=True, pattern=".مؤقت (.*)"))
+@jmthon.on(events.NewMessage(outgoing=True, pattern="^.مؤقت (.*)"))
 async def spammer(event):
     reply = await event.get_reply_message()
     input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
